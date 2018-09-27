80e7cda3ac9091faa6ff01d471dc2cfec0aa44c5
Will <willavawang@gmail.com>
2018-09-26 19:20:45 -0700

    Initial Commit

---
+++ Selfigram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Selfigram
+//
+//  Created by Will Ava Wang on 2018-09-26.
+//  Copyright © 2018 Will Ava Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
